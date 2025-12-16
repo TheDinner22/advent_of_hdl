49f16e47076881c2f4000eb294b226998dc7341c
Joseph Handsome <dontemailme@nah.com>
2025-12-16 00:31:19 -0500

    tb runs but
    i have no idea what the waveform is doing :(
    
    the non-waveform test is passing tho :)

---
+++ test/test_parse_ascii.ml
@@ -0,0 +1,74 @@
+open! Core
+open! Hardcaml
+open! Hardcaml_waveterm
+open! Hardcaml_test_harness
+
+module Parse_ascii = Aoc_day1.Parse_ascii
+
+module Harness =
+  Cyclesim_harness.Make (Parse_ascii.I) (Parse_ascii.O)
+
+let ( <--. ) = Bits.( <--. )
+
+let create (_scope : Scope.t) (i : Signal.t Parse_ascii.I.t) =
+  Parse_ascii.create i
+
+let simple_testbench
+    ~(inputs : Bits.t ref Parse_ascii.I.t)
+    ~(outputs : Bits.t ref Parse_ascii.O.t)
+    (sim : Harness.Sim.t)
+  =
+  let cycle () = Cyclesim.cycle sim in
+
+  let bits_of_ascii str =
+    assert (String.length str = 4);
+    let open Bits in
+    concat_msb (List.init 4 ~f:(fun i -> of_int_trunc ~width:8 (Char.to_int str.[i]))) in
+  let set raw_ascii =
+    inputs.clear <--. 1;
+    cycle ();
+    inputs.clear <--. 0;
+    inputs.din := (bits_of_ascii raw_ascii); 
+    cycle ();
+    cycle ();
+    cycle ();
+    cycle ();
+    cycle ();
+    let r = Bits.to_int_trunc !(outputs.was_r) in
+    let number = Bits.to_int_trunc !(outputs.number) in
+    print_s [%message "pipeline result" (raw_ascii : string) (r : int) (number : int)]
+  in
+
+  set "R123";
+;;
+
+let%expect_test "AND gate truth table" =
+  Harness.run ~create simple_testbench;
+  [%expect
+    {||}]
+;;
+
+let%expect_test "AND gate with printed waveforms" =
+  let display_rules =
+    [ Display_rule.port_name_matches
+        ~wave_format:(Bit_or Unsigned_int)
+        (Re.Glob.glob "*" |> Re.compile)
+    ]
+  in
+
+  Harness.run
+    ~create
+    ~trace:`All_named
+    ~print_waves_after_test:(fun waves ->
+      Waveform.print
+        ~display_rules
+        ~signals_width:20
+        ~display_width:80
+        ~wave_width:2
+        waves)
+    simple_testbench;
+
+  [%expect
+    {||}]
+;;
+
