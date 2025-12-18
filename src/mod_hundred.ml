70b3ba26e1227a67c0477edad6b7a4dadd6ac19f
Joseph Handsome <dontemailme@nah.com>
2025-12-17 22:18:12 -0500

    working comb mod operation

--- src/mod_hundred.ml
+++ src/mod_hundred.ml
@@ -1,6 +1,6 @@
-(*open Core*)
+open Core
 open Hardcaml
-(*open Signal*)
+open Signal
 
 module I = struct
   type 'a t =
@@ -17,8 +17,44 @@ end
 
 (* we assume all inputs are 4 chars for now, we can handle other cases later *)
 let create _scope ({ din } : _ I.t) : _ O.t =
+    (*first we need some logic to *)
+    let din_is_pos = din >=+ (of_int_trunc ~width:16 0) in (* i'd expect this not to synthesize into a comparitor *)
+    let pos_sel = List.map ~f:(of_int_trunc ~width:16) [100; 200; 300; 400; 500; 600; 700; 800; 900; 1000]
+              |> List.map ~f:(fun num -> din >=: num)
+              |> Signal.concat_msb in
+    let pos_mod_result = priority_select_with_default
+        ~default:din
+        With_valid.
+            [ { valid = pos_sel.:(0); value = din -: (of_int_trunc ~width:16 1000) }
+            ; { valid = pos_sel.:(1); value = din -: (of_int_trunc ~width:16 900) }
+            ; { valid = pos_sel.:(2); value = din -: (of_int_trunc ~width:16 800) }
+            ; { valid = pos_sel.:(3); value = din -: (of_int_trunc ~width:16 700) }
+            ; { valid = pos_sel.:(4); value = din -: (of_int_trunc ~width:16 600) }
+            ; { valid = pos_sel.:(5); value = din -: (of_int_trunc ~width:16 500) }
+            ; { valid = pos_sel.:(6); value = din -: (of_int_trunc ~width:16 400) }
+            ; { valid = pos_sel.:(7); value = din -: (of_int_trunc ~width:16 300) }
+            ; { valid = pos_sel.:(8); value = din -: (of_int_trunc ~width:16 200) }
+            ; { valid = pos_sel.:(9); value = din -: (of_int_trunc ~width:16 100) }
+            ] in
+    let neg_sel = List.map ~f:(of_int_trunc ~width:16) [-100; -200; -300; -400; -500; -600; -700; -800; -900]
+              |> List.map ~f:(fun num -> din <=+ num)
+              |> Signal.concat_msb in
+    let neg_mod_result = priority_select_with_default
+        ~default:(din +: (of_int_trunc ~width:16 100))
+        With_valid.
+            [ { valid = neg_sel.:(0); value = din +: (of_int_trunc ~width:16 1000) }
+            ; { valid = neg_sel.:(1); value = din +: (of_int_trunc ~width:16 900) }
+            ; { valid = neg_sel.:(2); value = din +: (of_int_trunc ~width:16 800) }
+            ; { valid = neg_sel.:(3); value = din +: (of_int_trunc ~width:16 700) }
+            ; { valid = neg_sel.:(4); value = din +: (of_int_trunc ~width:16 600) }
+            ; { valid = neg_sel.:(5); value = din +: (of_int_trunc ~width:16 500) }
+            ; { valid = neg_sel.:(6); value = din +: (of_int_trunc ~width:16 400) }
+            ; { valid = neg_sel.:(7); value = din +: (of_int_trunc ~width:16 300) }
+            ; { valid = neg_sel.:(8); value = din +: (of_int_trunc ~width:16 200) }
+            ] in
+    let mod_result = mux2 din_is_pos pos_mod_result neg_mod_result in
     {
-        dout = din;
+        dout = mod_result;
     }
 
 (* The [hierarchical] wrapper is used to maintain module hierarchy in the generated
