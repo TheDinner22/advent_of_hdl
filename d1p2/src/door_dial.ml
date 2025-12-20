635b97d2518f3ac4cc71e2b216bbf1e16f2109eb
Joseph Handsome <dontemailme@nah.com>
2025-12-19 23:10:56 -0500

    evil bug 60 inputs in
    still debugging

--- d1p2/src/door_dial.ml
+++ d1p2/src/door_dial.ml
@@ -26,16 +26,17 @@ let create scope ({ din; r; valid; clock; clear } : _ I.t) : _ O.t =
     let%hw dial_reg_out = Signal.wire 16 in
     let%hw counter_reg_out = Signal.wire 16 in
 
-    let value_to_mod = mux2 r (dial_reg_out +: din) ( dial_reg_out -: din) in
+    let%hw value_to_mod = mux2 r (dial_reg_out +: din) ( dial_reg_out -: din) in
     let mod_100_scope = Scope.sub_scope scope "mod100" in
     let mod_out = 
         Mod_hundred.hierarchical 
             mod_100_scope 
             { din = value_to_mod } in
 
-    let dial_reg_zero = dial_reg_out ==: of_int_trunc ~width:16 0 in
-    let would_double_count = dial_reg_zero &: (any_bit_set mod_out.times_passed_zero) in
-    let next_couter_reg = (counter_reg_out -: (uresize ~width:16 would_double_count) +: (uresize ~width:16 dial_reg_zero) +: (uresize ~width:16 mod_out.times_passed_zero)) in
+    let%hw mod_out_zero = mod_out.dout ==: of_int_trunc ~width:16 0 in
+    let%hw dial_reg_zero = dial_reg_out ==: of_int_trunc ~width:16 0 in
+    let%hw would_double_count = (mod_out_zero |: dial_reg_zero) &: (any_bit_set mod_out.times_passed_zero) in
+    let next_couter_reg = (counter_reg_out -: (uresize ~width:16 would_double_count) +: (uresize ~width:16 mod_out_zero) +: (uresize ~width:16 mod_out.times_passed_zero)) in
 
     let dr = Signal.reg spec ~enable:valid ~clear_to:(of_int_trunc ~width:16 50) mod_out.dout in
     let cr = Signal.reg spec ~enable:valid next_couter_reg in
